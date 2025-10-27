5f579a452f40d247e03b2934a88590cbc3df1207
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2025-10-27 17:21:15 +0300

    ArrayCreator finished

--- src/main/by/java/course/creator/ArrayCreator.java
+++ src/main/by/java/course/creator/ArrayCreator.java
@@ -1,20 +1,28 @@
 package main.by.java.course.creator;
 
+import main.by.java.course.exception.MyArrayException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
-
 public class ArrayCreator {
-    private static Logger logger = LogManager.getLogger();
-    private static String[] array;
+    private static final String ELEMENT_REGEX = "\\s+";
 
-    public static String[] factoryArray(List<String> strings) {
-        String[] result = new String[strings.size()];
+    private static final Logger logger = LogManager.getLogger();
 
-        for (int i = 0; i < strings.size(); i++) {
-            result[i] = strings.get(i);
+    public String[] createArray(String string) throws MyArrayException {
+        logger.debug("Splitting string into array: '{}'", string);
+
+        if (string == null) {
+            throw new MyArrayException("Input string cannot be null");
         }
+
+        if (string.isBlank()) {
+            logger.warn("Input string is blank, returning empty array");
+            return new String[0];
+        }
+
+        String[] result = string.split(ELEMENT_REGEX);
+        logger.debug("Successfully split string into {} elements", result.length);
 
         return result;
     }
